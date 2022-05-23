7ea576a5b4df2291d887a2bc5799278f74b90002
juampimas <juampim98@gmail.com>
2022-05-23 15:09:19 -0300

    arreglos

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'
 function ItemList({items}) {
   return (
     
-      items.map((prod) => <Item id={prod.id} nombre={prod.nombre} categoria={prod.categoria} precio={prod.precio} img={prod.img} stock={prod.stock}/>)
+      items.map((prod) => <Item key={prod.id} id={prod.id} nombre={prod.nombre} categoria={prod.categoria} precio={prod.precio} img={prod.img} stock={prod.stock}/>)
     
   )
 }
